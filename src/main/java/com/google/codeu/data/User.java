c042b991a2cb1418ede47b3ca440ec5fdf59b2a5
Mario Verdugo <mv7757@email.arizona.edu>
2019-04-15 20:56:32 -0700

    Added in a new field that is an arraylist of all the topics that user is associated with. Added in getter and setter methods for this field as well.

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -1,13 +1,24 @@
 package com.google.codeu.data;
+import java.util.ArrayList;
 
 public class User {
 
   private String email;
   private String aboutMe;
-
+  private ArrayList<String> topics;
+  
   public User(String email, String aboutMe) {
     this.email = email;
     this.aboutMe = aboutMe;
+    this.topics = new ArrayList<String>();
+  }
+
+  public void addTopic(String link){
+    this.topics.add(link);
+  }
+  
+  public ArrayList<String> getTopics(){
+    return this.topics;
   }
 
   public String getEmail(){
